a1eaee4f028fc7aae27db1dc6fe625df00b32c9e
el3omrii <133902257+el3omrii@users.noreply.github.com>
2026-07-30 16:57:56 +0100

    Add weekly storage cleanup command to schedule

--- routes/console.php
+++ routes/console.php
@@ -4,3 +4,4 @@ use App\Console\Commands\GrabEPGData;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command("app:grab-epg")->everyTwoHours();
+Schedule::command("storage:clean-old")->weekly();
