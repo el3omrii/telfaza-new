278a15d1d4eeb79e530e54e95df05f62fbff0cb8
el3omrii <133902257+el3omrii@users.noreply.github.com>
2026-08-03 11:11:32 +0000

    video sitemap optimizations

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -151,8 +151,9 @@ class SitemapController extends Controller
                 'title'       => $channel->name,
                 'description' => Str::limit(strip_tags($channel->description ?? ''), 160),
                 'thumbnail'   => $channel->image ? env('APP_STORAGE_URL') . $channel->image : null,
-                'content_loc' => $this->buildUrl($frontendUrl, '/channels/' . $channel->slug),
-                'stream_loc'  => $channel->sources->first()?->link,
+                'url'         => $this->buildUrl($frontendUrl, '/channels/' . $channel->slug),
+                'content_loc' => $this->channel->sources->first()?->link,
+                'stream_loc'  => $this->buildUrl($frontendUrl, '/embed/' . $channel->slug),
                 'publication' => $channel->created_at?->toAtomString(),
             ];
         }
--- resources/views/sitemaps/video-xml.blade.php
+++ resources/views/sitemaps/video-xml.blade.php
@@ -3,13 +3,14 @@
         xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
     @foreach($videos as $video)
     <url>
-        <loc>{{ $video['content_loc'] }}</loc>
+        <loc>{{ $video['url'] }}</loc>
         @if(!empty($video['thumbnail']))
         <video:video>
             <video:title>{{ $video['title'] }}</video:title>
             <video:description>{{ $video['description'] }}</video:description>
             <video:thumbnail_loc>{{ $video['thumbnail'] }}</video:thumbnail_loc>
-            <video:content_loc>{{ $video['stream_loc'] }}</video:content_loc>
+            <video:content_loc>{{ $video['content_loc'] }}</video:content_loc>
+            <video:player_loc>{{ $video['stream_loc'] }}</video:player_loc>
             @if(!empty($video['publication']))<video:publication_date>{{ $video['publication'] }}</video:publication_date>@endif
             <video:live>yes</video:live>
         </video:video>
