31cce5aabb850af8843f0e7fc4a80e0cff68083f
el3omrii <133902257+el3omrii@users.noreply.github.com>
2026-08-13 20:29:00 +0100

    Add ClearsNextjsCache trait to Country model

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Traits\ClearsNextjsCache;
 
 class Country extends Model
 {
+    use ClearsNextjsCache;
     protected $fillable = [
         'name',
         'flag',
